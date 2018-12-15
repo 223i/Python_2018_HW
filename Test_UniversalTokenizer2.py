08a4d3b639465041e5672f1537bf95ad0692e50e
223i <ironkorobeinikova@gmail.com>
2018-12-15 23:26:22 +0300

    Universal tokenizer and tests for it

--- Test_UniversalTokenizer2.py
+++ Test_UniversalTokenizer2.py
@@ -1,12 +1,38 @@
 """This module contains a set of tests for 'UniversalTokenizer2.py' module"""
 
+
 import unittest
-from UniversalTokenizer2 import  UniversalTokenizer
+from UniversalTokenizer2 import Token, UniversalTokenizer
 
 
 class UniversalTokenizerTest(unittest.TestCase):
     """ The basic class that inherits unittest.TestCase. It contains
     the main tests for module "UniversalTokenizer2.py" """
+
+    def test_type_of_symbol_digit(self):
+        """check whether the returned value is digit."""
+        a = UniversalTokenizer().type_define('4')
+        self.assertEqual(a, 'Digit')
+
+    def test_type_of_symbol_alpha(self):
+        """check whether the returned value is alphabetical symbol."""
+        a = UniversalTokenizer().type_define('a')
+        self.assertEqual(a, 'Alpha')
+
+    def test_type_of_symbol_space(self):
+        """check whether the returned value is whitespace."""
+        a = UniversalTokenizer().type_define(' ')
+        self.assertEqual(a, 'Space')
+
+    def test_type_of_symbol_punctuation(self):
+        """check whether the returned value is punctuation."""
+        a = UniversalTokenizer().type_define('!')
+        self.assertEqual(a, 'Punctuation')
+
+    def test_type_of_symbol_othertype(self):
+        """check whether the returned value is other type."""
+        a = UniversalTokenizer().type_define('+')
+        self.assertEqual(a, 'Other type')
 
     def test_tokenize_wrong_type_of_input_fail(self):
         """check the return value if an input is not a string"""
@@ -15,49 +41,46 @@ class UniversalTokenizerTest(unittest.TestCase):
 
     def test_tokenize_empty_string_input(self):
         """check the return value if an input is an empty string"""
-        a = UniversalTokenizer().tokenize("")
+        a = UniversalTokenizer().tokenize('')
         self.assertEqual(a, [])
 
     def test_tokenize_one_alphabetical_symbol_input(self):
-        """check the returned value if an input is one alphabetical symbol."""
-        actual_result = UniversalTokenizer().tokenize("a")
+        """check the returned token if an input is alphabetical symbol."""
+        actual_result = UniversalTokenizer().tokenize('word')
         instance_of_actual_result = actual_result[0]
+        self.assertEqual(instance_of_actual_result.type_of_token, 'Alpha')
+        self.assertEqual(instance_of_actual_result.string_representation,
+                         'word')
         self.assertEqual(instance_of_actual_result.position, 0)
-        self.assertEqual(instance_of_actual_result.string_representation, 'a')
 
     def test_tokenize_space_input(self):
-        """check the return value if an input is a whitespace"""
-        actual_result = UniversalTokenizer().tokenize(" ")
+        """check the returned token if an input is a whitespace"""
+        actual_result = UniversalTokenizer().tokenize(' ')
         instance_of_actual_result = actual_result[0]
+        self.assertEqual(instance_of_actual_result.type_of_token,
+                         'Space')
+        self.assertEqual(instance_of_actual_result.string_representation,
+                         ' ')
         self.assertEqual(instance_of_actual_result.position, 0)
-        self.assertEqual(instance_of_actual_result.string_representation, " ")
 
     def test_tokenize_digit_input(self):
-        """check the returned value if an input is one digit"""
-        actual_result = UniversalTokenizer().tokenize("4")
+        """check the returned token if an input is one digit"""
+        actual_result = UniversalTokenizer().tokenize('1')
         instance_of_actual_result = actual_result[0]
+        self.assertEqual(instance_of_actual_result.type_of_token, 'Digit')
+        self.assertEqual(instance_of_actual_result.string_representation,
+                         '1')
         self.assertEqual(instance_of_actual_result.position, 0)
-        self.assertEqual(instance_of_actual_result.string_representation, '4')
 
     def test_tokenize_punctuation_input(self):
-        """check the returned value if an input is punctuation symbol."""
-        actual_result = UniversalTokenizer().tokenize("!")
+        """check the returned token if an input is punctuation symbol."""
+        actual_result = UniversalTokenizer().tokenize('!')
         instance_of_actual_result = actual_result[0]
+        self.assertEqual(instance_of_actual_result.type_of_token,
+                         'Punctuation')
+        self.assertEqual(instance_of_actual_result.string_representation,
+                         '!')
         self.assertEqual(instance_of_actual_result.position, 0)
-        self.assertEqual(instance_of_actual_result.string_representation, "!")
-
-    def test_tokenizer_space_after_punctuation(self):
-        """check the returned value if the input has whitespace
-         after punctuation."""
-        actual_result = UniversalTokenizer().tokenize('token. Token')
-        values_for_comparison = [0, 'token',5, '.', 6, ' ', 7, 'Token']
-        p = 0  #counter for list
-        for i in actual_result:
-            self.assertEqual(i.position, values_for_comparison[p])
-            p += 1
-            self.assertEqual(i.string_representation,
-                             values_for_comparison[p])
-            p += 1
 
 
 if __name__ == '__main__':
---
+++ UniversalTokenizer2.py
@@ -0,0 +1,106 @@
+"""This module contains two classes for text-tokenisation"""
+
+import unicodedata
+
+
+class Token:
+    """ Form new data-type "token" with 3 parameters - type_of_token,
+    string_representation and position. """
+
+    def __init__(self, type_of_token, string_representation, position):
+        """ Inits Token with 3 parameters:
+        :param type_of_token: type of token
+        :param string_representation: sequence named token.
+        :param position: position of first symbol in token.
+        """
+        self.type_of_token = type_of_token
+        self.string_representation = string_representation
+        self.position = position
+
+
+class UniversalTokenizer:
+    """ This class provides a method for text-tokenization.
+        For the text submitted the class return the result of tokenization
+        as a list of tokens.
+
+        For instance: The string "I am Groot!" will be tokenized by
+        the following manner:
+        [Alpha I 0
+        Space    1
+        Alpha am 2
+        Space    4
+        Alpha Groot 5
+        Punctuation ! 10]
+
+        """
+
+
+
+    def type_define (self, input):
+        """ Read the string, extract the sequences and return their types.
+
+        :param string: input line, the appropriate type - string
+        :return: list of tokens
+
+        """
+        if input.isdigit():
+            return 'Digit'
+        if input.isalpha():
+            return 'Alpha'
+        if input.isspace():
+            return 'Space'
+        if unicodedata.category(input) == 'Po':
+            return 'Punctuation'
+        else:
+            return 'Other type'
+
+
+    def tokenize(self, input):
+        """ Read the string, extract the different types of sequences and
+         their starting positions and return them in a list of tokens.
+
+        :param input: input line, the appropriate type - string
+        :return: list of tokens
+
+        """
+
+        if not isinstance(input, str):
+            raise TypeError('Invalid type of input')
+
+        tokens = []  # list for tokens
+        """Check whether the input is an empty string"""
+
+        if len(input) == 0:
+            return tokens
+
+        for idSymbol, symbol in enumerate(input):
+            """ The next conditions check when the type of next
+            symbol changes and create new token with his characteristics"""
+
+            type = UniversalTokenizer().type_define(symbol)
+            if idSymbol == 0:
+                start_token = idSymbol
+            elif UniversalTokenizer().type_define(input[idSymbol - 1]) == type:
+                continue
+            elif UniversalTokenizer().type_define(input[idSymbol-1]) != type:
+                tokens.append(Token(UniversalTokenizer().type_define
+                                    (input[idSymbol - 1]),
+                                    start_token, input[start_token:idSymbol]))
+                start_token = idSymbol
+
+        """ This condition checks the last symbol and writes down 
+        last token and his position"""
+
+        if UniversalTokenizer().type_define(input[idSymbol]) == type:
+            tokens.append(Token(UniversalTokenizer().type_define
+                                (input[idSymbol]), start_token,
+                                input[start_token:idSymbol+1]))
+        return tokens
+
+
+if __name__ == '__main__':
+    text = 'word'
+    lst = UniversalTokenizer().tokenize(text)
+    for token in lst:
+        print(token.type_of_token, ':', token.position,
+              ':', token.string_representation)
