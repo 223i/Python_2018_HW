2483b018b4b943778e9dbef2f2405cbfb4015b4c
Irina Korobeynikova <33998625+223i@users.noreply.github.com>
2019-01-10 09:14:23 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,62 +1,6 @@
 "This module parses text and returns possible syntax tree-options"""
 
-class Token:
-    """ Form new data-type "token" with 2 parameters - string_representation
-        and position. """
-
-    def __init__(self, string_representation, position):
-        self.string_representation = string_representation
-        self.position = position
-
-
-class Tokenizer:
-    """ This class provides a method for text-tokenization.
-       For the text submitted the class return the result of tokenization
-       as a list of tokens.
-       For instance: The string "I am Groot" will be tokenized by
-       the following manner:
-       [0 I
-       2 am
-       5 Groot]
-       """
-
-    def tokenize(self, string):
-        """ Read the string, extract the alphabetical sequences and their
-        starting positions and return them in a list of tokens.
-        :param string: input line, the appropriate type - string
-        :return: list of tokens
-        """
-
-        if not isinstance(string, str):
-            raise TypeError('Invalid type of input')
-
-        tokens = []  # list for tokens
-
-        # Check whether the input is an empty string
-        if len(string) == 0:
-            return tokens
-        for idSymbol, symbol in enumerate(string):
-            # The next 3 conditions check those cases, when the first symbols
-            # in the string are nonalphabetical, when symbol is the beginning
-            # of the alphabetical sequence or the first word in the line
-            # and when, and when symbol is the end of alphabetical sequence
-
-            if not symbol.isalpha() and (idSymbol == 0
-                                         or not string[idSymbol - 1].isalpha()):
-                continue
-
-            elif symbol.isalpha() and (idSymbol == 0
-                                       or not string[idSymbol - 1].isalpha()):
-                start = idSymbol
-
-            elif not symbol.isalpha() and string[idSymbol - 1].isalpha():
-                tokens.append(Token(string[start:idSymbol], start))
-
-        # This condition checks the last symbol. If this symbol is a alpha,
-        # method writes down last token and his position
-        if string[-1].isalpha():
-            tokens.append(Token(string[start:len(string)], start))
-        return tokens
+from Tokenizer1 import Token, Tokenizer
 
 
 class Constituent:
@@ -126,17 +70,11 @@ class Parser:
             old_constituent = self.identity_constituent[identity]
             old_constituent[identity] = structures.extend(constituent.structures)
 
-        def bind(self, c):
+    def bind(self, c):
         end_of_previous = self.right_border_constituent[c.start-1]
         for end_of_previous in self.right_border_constituent:
             n = self.right_border_constituent.get(end_of_previous)
-            if (n.tag) in grammar:
-                for i in grammar[(n.tag)]:
-                    t = Constituent(i, n.start, c.end, [(n.tag)])
-                    print(i, n.start, n.end)
-                    self.put(t)
             if (n.tag, c.tag) in grammar:
-                print(n.tag, c.tag)
                 for i in grammar[(n.tag, c.tag)]:
                     t = Constituent(i, n.start, c.end, [(n.tag, c.tag)])
                     self.put(t)
