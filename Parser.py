0fa19c56c75e83f550cfb8585d77f4ad49d5425f
Irina Korobeynikova <33998625+223i@users.noreply.github.com>
2019-01-10 10:01:17 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -8,6 +8,12 @@ class Constituent:
     string_representation, start, end, structures. """
 
     def __init__(self, tag, start, end, structures):
+        """ Inits Conctituent with 4 parameters:
+        :param tag: tag of constituent
+        :param start: the beginning of constituent.
+        :param end: the end of constituent.
+        :param structures: structures for this token
+        """
         self.tag = tag
         self.start = start
         self.end = end
@@ -23,17 +29,26 @@ class TokenWithTag:
     """ Reorganizes data-type "token" it means adding a parameter - tag """
 
     def __init__(self, string_representation, position, tag):
+        """ Inits Conctituent with 4 parameters:
+        :param string_representation: sequence named token.
+        :param position: the beginning of token.
+        :param tag: the tag of token.
+        """
         self.string_representation = string_representation
         self.position = position
         self.tag = tag
 
 
-class Morphoanalyzer:  # tokenize and add POS-tags
+class Morphoanalyzer:
     """ This class provides a method for morphological analysis.
        For the text submitted the class return the result of morphological
         analysis as a list of tokens. """
 
     def dumm_morphoanalyzer(self, input):
+        """ Tokenizes the string. For each token reorganizes it's structure
+        :param string: input line, the appropriate type - string
+        :return: list of tokens with tags
+        """
         tokens = Tokenizer().tokenize(input)
         tokens_with_tags = []
         for i in tokens:
@@ -50,27 +65,53 @@ class Morphoanalyzer:  # tokenize and add POS-tags
 if __name__ == '__main__':
     lst = Morphoanalyzer().dumm_morphoanalyzer("мама мыла раму")
     for token in lst:
-        print(token.position, ': ', token.string_representation, ': ', token.tag)
+        print(token.position, ': ', token.string_representation, ': ',
+         token.tag)
 """
 
 
 class Parser:
+    """ This class provides a methods for text-parsing.
+        For the text submitted the class returns the result of parsing
+        as a set of constituents. """
 
     def __init__(self):
+        """ Inits 2 dictionaries: right_border_constituent and
+         identity_constituent
+        """
         self.right_border_constituent = {}
         self.identity_constituent = {}
 
     def add(self, constituent):
+        """ For the constituent submitted the method checks whether parameters
+         of constituent are in the dictionary 'identity_constituent'. For
+         those cases, when it is, structures of constituent are updated.
+         Otherwise, constituent is added in both dictionaries and is returned.
+
+        :param constituent: submitted constituent
+        :return: constituent, that is added in dictionaries
+        """
+
         identity = (constituent.start, constituent.end, constituent.tag)
         if identity not in self.identity_constituent.keys():
             self.right_border_constituent[constituent.end] = constituent
-            self.identity_constituent[(constituent.start, constituent.end, constituent.tag)] = constituent
+            self.identity_constituent[(constituent.start, constituent.end,
+                                       constituent.tag)] = constituent
             return constituent
         else:
             old_constituent = self.identity_constituent[identity]
-            old_constituent[identity] = structures.extend(constituent.structures)
+            old_constituent[identity] = structures.extend(
+                constituent.structures)
 
     def bind(self, c):
+        """For the constituent submitted the method checks whether previous
+         constituent and constituent submitted are in grammar. For each
+         case, when they are, method builds new constituent and submit
+         it to "put()" function.
+
+        :param c: submitted constituent
+        """
+
         end_of_previous = self.right_border_constituent[c.start-1]
         for end_of_previous in self.right_border_constituent:
             n = self.right_border_constituent.get(end_of_previous)
@@ -80,14 +121,23 @@ class Parser:
                     self.put(t)
 
     def put(self, constituent):
+        """This method becomes constituent and calls the method 'add()' for it.
+        If returned value is not None, it calls the method 'bind()' for
+         constituent.
+
+        :param constituent:submitted constituent
+        """
         result = self.add(constituent)
         if result != None and constituent.start != 0:
             self.bind(constituent)
 
     def parse(self, string):
-        # получаем список с токенами у которых известна
-        # начальная позиция, сам токен,и его частеречная принадлежность.
-        # Токены указаны в словаре в той же последовательности, в какой идут в тексте
+        """This method becomes string, provides morphoanalysis for it. Method creates
+        constituent for each morphoanalyzed and calls  "put()" function for them.
+
+        :param string: input string
+        """
+
         tokens_for_work = Morphoanalyzer().dumm_morphoanalyzer(string)
 
         for token in tokens_for_work:
